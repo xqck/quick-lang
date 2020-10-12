5b95fd10f6685f3a6d479c594879a2dcde248949
kev2243 <47433165+kev2243@users.noreply.github.com>
2020-10-12 20:17:45 +0100

    formatted the code and added some common files and functions

---
+++ quick-lang/common.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// headers that are not mine
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <map>
+#include <vector>
+
+// defines
+#define wl(x) std::cout << x << std::endl;
+#define wn(x) std::cout << x;
+
+// headers that I created.
+#include "commonfunctions.h"
